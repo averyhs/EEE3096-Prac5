7b310cd0cf4d1424eb7edea9e632c8769acba2da
hrrhan002 <hrrhan002@myuct.ac.za>
2020-10-29 18:09:25 +0200

    Button toggles sample rate

--- p5-ldr.py
+++ p5-ldr.py
@@ -1,3 +1,4 @@
+import RPi.GPIO as GPIO
 import threading
 import busio
 import digitalio
@@ -6,36 +7,53 @@ import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 from time import time
 
-# create the spi bus
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+# Define global variables
+chan = None # SPI channel
+thread = None # thread of print_adc_thread
+sample_rates = [1, 5, 10] # list of sample rates to toggle through
+sr_idx = 0 # start with sample rate of 1s
 
-# create the cs (chip select)
-cs = digitalio.DigitalInOut(board.D5)
-# create the mcp object
-mcp = MCP.MCP3008(spi, cs)
+# Function to set up SPI for the ADC
+def spi_setup():
+    global chan
+    spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI) # create the spi bus
+    cs = digitalio.DigitalInOut(board.D5) # create the cs (chip select)
+    mcp = MCP.MCP3008(spi, cs) # create the mcp object
+    chan = AnalogIn(mcp, MCP.P1) # create an analog input channel on pin 1
 
-# create an analog input channel on pin 1
-chan = AnalogIn(mcp, MCP.P1)
+# Function to set up gpio for pushbutton
+def gpio_setup():
+    GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set button on pin18 as input with pullup resistor
+    GPIO.add_event_detect(24, GPIO.FALLING, callback=toggle_samp_rate, bouncetime=500) # set callback & debounce
 
-# Function to print sensor value every 5 seconds
-def print_adc_thread():
-    thread = threading.Timer(5.0, print_adc_thread) # execute every 5s
-    thread.daemon = True # Exit thread when program does
+# Function to print sensor value every 10 seconds
+def print_adc():
+    global chan, thread, sample_rates, sr_idx
+    thread = threading.Timer(sample_rates[sr_idx], print_adc) # execute every <interval> seconds
+    thread.daemon = True # Stop thread if program stops
     thread.start()
 
-    runtime = time() - starttime # calculate runtime
-    runtime = int(runtime)
-    
-    # Calculate light
-    ldr_voltage = 3.3 - chan.voltage
+    runtime = int(time() - starttime) # calculate runtime & cast to int
+    ldr_voltage = 3.3 - chan.voltage # calculate LDR voltage
+    print(runtime,"s\t",chan.value,"\t\t",ldr_voltage," V",sep="") # print ADC value
 
-    # print adc value
-    print(runtime,"s\t",chan.value,"\t\t",ldr_voltage," V",sep="")
+# Callback fn for button to toggle sampling rate
+def toggle_samp_rate(channel):
+    global thread, sample_rates, sr_idx
+    sr_idx = (sr_idx + 1) % 3
+    
+    if thread.is_alive():
+        thread.cancel() # cancel currently running thread
+        print_adc()
 
 if __name__ == "__main__":
-    starttime = time() # get start time
+    spi_setup()
+    gpio_setup()
+    
     print("Runtime\tADC Reading\tLDR Voltage")
-    print_adc_thread() # start print thread
+    starttime = time() # get start time
+    print_adc()
 
-    while time() - starttime < 35:
-        pass # run for 30 sec
+    while time() - starttime < 61:
+        pass # run for 1 min
+
