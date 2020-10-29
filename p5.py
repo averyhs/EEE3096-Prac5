78561f4cb9eee17b30d9524ac508be4ea2f313ff
hrrhan002 <hrrhan002@myuct.ac.za>
2020-10-29 13:37:33 +0200

    Change to pin 1
    Circuit has thermistor at pin 1 so changed code to fix that. Also removed unnecessary cast to int in while condition

--- p5.py
+++ p5.py
@@ -14,8 +14,8 @@ cs = digitalio.DigitalInOut(board.D5)
 # create the mcp object
 mcp = MCP.MCP3008(spi, cs)
 
-# create an analog input channel on pin 0
-chan = AnalogIn(mcp, MCP.P0)
+# create an analog input channel on pin 1
+chan = AnalogIn(mcp, MCP.P1)
 
 # Function to print sensor value every 10 seconds
 def print_adc_thread():
@@ -41,5 +41,5 @@ if __name__ == "__main__":
     print("Runtime\tTemp Reading\tTemp")
     print_adc_thread() # start print thread
 
-    while int(time() - starttime) < 65:
+    while time() - starttime < 65:
         pass # run for 1 min
